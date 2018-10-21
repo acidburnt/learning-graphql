4ca09c35dbf2644df6cbda1327ecae2482db1883
Michal Szydlowski <michal.szydlowski@netguru.co>
2018-10-21 23:35:43 +0200

    Add authors

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const schema = require('./shema/schema');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({ schema }));
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(4000, () => {
   console.log('Server started at port 4000.');
--- server/shema/schema.js
+++ server/shema/schema.js
@@ -1,7 +1,9 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt,
+} = graphql;
 
 const books = [
   { id: '1', name: 'Name of the wind', genre: 'Fantasy' },
@@ -9,12 +11,27 @@ const books = [
   { id: '3', name: 'Ubik', genre: 'Sci-fi' },
 ];
 
+const authors = [
+  { id: '1', name: 'Miro Maro', age: 23 },
+  { id: '2', name: 'Andrzej Dudka', age: 34 },
+  { id: '3', name: 'Czesław Kowalski', age: 98 },
+];
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -23,10 +40,17 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // code to get data from db / other source
         return _.find(books, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id });
       },
     },
   },
