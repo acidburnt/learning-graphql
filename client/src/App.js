693a9f5830fe0576987502d609a9be9a1795dc64
Michal Szydlowski <michal.szydlowski@netguru.co>
2018-10-23 19:02:27 +0200

    Setup eslint in client folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
-import React, { Component } from "react";
-import { BookList } from "./components/BookList";
+import React from 'react';
+import { BookList } from './components/BookList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Reading List</h1>
-        <BookList />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <h1>Reading List</h1>
+    <BookList />
+  </div>
+);
 
 export default App;
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 export const BookList = () => (
   <ul className="book-list">
